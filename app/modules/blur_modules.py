85502d9642b46346dc9536713ed581b2c2ab4e3a
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2020-01-17 14:59:12 +0000

    feat(modules): refactor blur modules

---
+++ app/modules/blur_modules.py
@@ -0,0 +1,58 @@
+"""
+Geocoder Module
+@author Irfan Andriansyah <irfan@99.co>
+"""
+import os
+from string import Template
+from PIL import Image, ImageFilter
+
+
+class BlurModules:
+    """
+    Blur Module
+    """
+
+    IMAGE = ''
+    BACKGROUND = None
+
+    def __init__(self, images):
+        """
+        Constructor
+        :param images: Str
+        """
+        self.IMAGE: str = os.path.abspath(os.getcwd()) + '/upload/' + images
+        self.BACKGROUND = self.setterBackground()
+    
+    def setterBackground(self):
+        """
+        Setter background image
+        """
+        return Image.open(self.IMAGE)
+    
+    def setterBlur(self, image):
+        """
+        Setter background image
+        """
+        return image.filter(ImageFilter.GaussianBlur(radius=5))
+    
+    def setterPath(self):
+        base_filename = os.path.basename(self.IMAGE)
+        template = Template('blur-image-$base_filename')
+        path = template.substitute(base_filename=base_filename)
+
+        return {
+            "path": path,
+            "full_path": os.path.abspath(os.getcwd()) + '/upload/' + path,
+        }
+    
+    def run(self):
+        blurImage = self.setterBlur(self.BACKGROUND)
+
+        if blurImage.mode in ('RGBA', 'LA'):
+            jpeg_blur = Image.new(blurImage.mode[:-1], blurImage.size, '#ffffff')
+            jpeg_blur.paste(blurImage, blurImage.split()[-1])
+            blurImage = jpeg_blur
+        
+        blurImage.save(self.setterPath().get('full_path'), 'JPEG')
+
+        return self.setterPath().get('path')
--- app/router/blur.py
+++ app/router/blur.py
@@ -4,18 +4,9 @@ Blur Image enpoint api
 """
 import os
 from app import photos
-from string import Template
-from PIL import Image, ImageFilter
 from flask import Blueprint, request
 from app.utils.response import responseAPIHelper
-
-fill_color = '#69acec'  # your background
-
-def landscapeDimensions(size):
-    width, height = size
-
-    return width, height
-
+from app.modules.blur_modules import BlurModules
 
 
 mod = Blueprint('blur', __name__, url_prefix='/blur')
@@ -26,28 +17,16 @@ def upload_images():
         if request.method == 'POST' and 'photo' in request.files:
             filename = photos.save(request.files['photo'])
             file_url = photos.url(filename)
-            image_path = os.path.abspath(os.getcwd()) + '/upload/' + filename
-            background = Image.open(image_path)
-            im_blur = background.filter(ImageFilter.GaussianBlur(radius=5))
-            
-            base_filename = os.path.basename(image_path)
-            template = Template('blur-image-$base_filename')
-            filenameBlur = os.path.abspath(os.getcwd()) + '/upload/' + template.substitute(base_filename=base_filename)
-
-            if im_blur.mode in ('RGBA', 'LA'):
-                jpeg_blur = Image.new(im_blur.mode[:-1], im_blur.size, fill_color)
-                jpeg_blur.paste(im_blur, im_blur.split()[-1])
-                im_blur = jpeg_blur
-
-            im_blur.save(filenameBlur, 'JPEG' )
+            blur_images = BlurModules(filename).run()
 
             return responseAPIHelper({
                 'original_image': file_url,
-                'blur_image': photos.url(template.substitute(base_filename=base_filename))
+                'blur_image': photos.url(blur_images)
             }, True), 200
         else:
             raise ValueError('Error Request')
     except BaseException as e:
+        print(e)
         return responseAPIHelper({
             'Error': str(e)
         }, False), 200
