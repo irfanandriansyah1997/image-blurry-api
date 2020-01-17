6b1faf00e469baaa13a8e60f27d98b33d25f2d16
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2020-01-17 14:19:48 +0000

    feat(requirement): update requirements filex

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 SERVER_IP = '0.0.0.0'
-SERVER_PORT = '8000'
+SERVER_PORT = int(os.environ.get('PORT', 33507))
 
 THREADS_PER_PAGE = 8
